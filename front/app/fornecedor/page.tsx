eb68e798bfd3ae66f2d52789cb6a82cfed7009d2
Henrique <henriquedeeealmeida@gmail.com>
2025-11-28 15:58:53 -0300

    TDD: adiciona teste inicial da página de fornecedor (falhando)

---
+++ front/app/fornecedor/page.tsx
@@ -0,0 +1,5 @@
+export default function FornecedorPage() {
+  return (
+    <h1>Fornecedor</h1>
+  );
+}
---
+++ front/tests/fornecedor.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Fornecedor from "../app/fornecedor/page";
+import '@testing-library/jest-dom';
+
+describe("Tela de Fornecedor", () => {
+
+  test("deve exibir o título 'Cadastro de Fornecedores'", () => {
+    render(<Fornecedor />);
+
+    expect(
+      screen.getByRole("heading", { name: /Cadastro de Fornecedores/i })
+    ).toBeInTheDocument();
+
+  });
+});
